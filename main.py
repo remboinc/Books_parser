1be478b0c0702e62ca85e7a35cf6b6c04695f577
remboinc <remboinc@mail.ru>
2023-11-09 12:31:24 +0300

    check_for_redirect

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 import requests
 from pathlib import Path
 
+from requests import HTTPError
+
+
+def check_for_redirect(response):
+    if response.history:
+        raise HTTPError('Redirect')
+    else:
+        pass
+
+
 
 def book_parser(url, books_path):
-    try:
-        ids = 1
-        while ids != 11:
-            params = {"id": ids}
-            response = requests.get(url, params=params)
-            response.raise_for_status()
-            filename = Path(books_path) / f"{ids}.txt"
-            with open(filename, 'wb') as file:
-                file.write(response.content)
-            ids += 1
-    except Exception:
-        print(f"Не удалось скачать книгу {filename}")
+    ids = 1
+    while ids <= 10:
+        params = {"id": ids}
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        check_for_redirect(response)
+        filename = Path(books_path) / f"{ids}.txt"
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+        ids += 1
 
 
 def main():
     books_path = "books"
     Path(books_path).mkdir(parents=True, exist_ok=True)
     url = "https://tululu.org/txt.php"
-    book_parser(url, books_path)
+
+    try:
+        book_parser(url, books_path)
+    except HTTPError:
+        print("Не удалось скачать книгу")
+
+
 
 
 if __name__ == '__main__':
